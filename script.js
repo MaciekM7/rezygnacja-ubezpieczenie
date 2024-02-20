6047adb75d17010ee8e0a4007bfc1c803e116ad5
Maciej Mora <maciek.mora@gmail.com>
2024-02-20 18:01:06 +0100

    Added validation

--- script.js
+++ script.js
@@ -36,3 +36,36 @@ function firmaVisible() {
     .getElementById("info-container")
     .classList.add("info-container-shorter");
 }
+
+function validate() {
+  const validationAlert = document.getElementById("validationAlert");
+  const radioFirma = document.getElementById("firmaRadio").checked;
+  const radioOsoba = document.getElementById("osobaRadio").checked;
+  const imie = document.getElementById("imie");
+  const nazwisko = document.getElementById("nazwisko");
+  const firma = document.getElementById("firma");
+  const regon = document.getElementById("regon");
+  const urodzenia = document.getElementById("urodzenia");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(email.value)) {
+    if (radioOsoba) {
+      if (imie.value != "" && nazwisko.value != "") {
+        window.location.href = "success.html";
+      } else {
+        validationAlert.innerText = "Błąd w podanych danych.";
+      }
+    } else if (radioFirma) {
+      if (firma.value != "" && regon.value != "") {
+        window.location.href = "success.html";
+      } else {
+        validationAlert.innerText = "Błąd w podanych danych.";
+      }
+    }
+  } else {
+    document.getElementById("validationAlert").innerText =
+      "Wpisany adres e-mail jest nieprawidłowy";
+  }
+}
